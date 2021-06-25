095a9c997827e4f01eb045be72b90b7b3fea09d4
kipishio <kipishio@gmail.com>
2021-06-25 13:07:08 +0500

    Выполнение домашней работы 015_2

--- 015/015_01/task_003/task_003.py
+++ 015/015_01/task_003/task_003.py
@@ -1,1 +1,16 @@
-print()
+count_worker = int(input("Кол-во сотрудников в офисе: "))
+list_worker_id = []
+
+for _ in range(count_worker):
+    id_worker = int(input("ID сотрудника: "))
+    list_worker_id.append(id_worker)
+
+search_id_worker = int(input("Какой ID ищем?: "))
+
+if search_id_worker in list_worker_id:
+    print("Сотрудник на месте")
+else:
+    print("Сотрудник не работает")
+
+
+
