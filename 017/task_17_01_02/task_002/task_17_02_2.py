805ac2b10371cc8cd49747480c3fbaca08f6a671
kipishio <kipishio@gmail.com>
2021-07-28 14:15:54 +0500

    Vipolnenie domashney raboti №17

--- 017/task_17_01_02/task_002/task_17_02_2.py
+++ 017/task_17_01_02/task_002/task_17_02_2.py
@@ -7,3 +7,8 @@
 '''
 
 str_in = input("Введите строку: ")
+str_symbol = input("Введите дополнительный символ: ")
+doubled_symbol = [i*2 for i in str_in]
+print("Список удвоенных символов: ", doubled_symbol)
+list_additional_symbol = [i + str_symbol for i in doubled_symbol]
+print("Склейка с дополнительным символом: ", list_additional_symbol)
