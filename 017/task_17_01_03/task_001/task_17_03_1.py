8203fc5519149d43909add19b68c1c1f291a06b9
kipishio <kipishio@gmail.com>
2021-07-28 14:15:54 +0500

    Vipolnenie domashney raboti №17

---
+++ 017/task_17_01_03/task_001/task_17_03_1.py
@@ -0,0 +1,12 @@
+import random
+
+first_int = int(input("Укажите первое число: "))
+second_int = int(input("Укажите второе число: "))
+i = 0
+list_square = [i for i in
+               [random.randint(first_int, second_int) for _ in range(10)]
+               if i % 2 == 0]
+print(list_square)
+
+li = [i for i in range(first_int, second_int) if i % 2 == 0]
+print(li)
--- 017/task_17_01_03/task_002/task_17_03_2.py
+++ 017/task_17_01_03/task_002/task_17_03_2.py
@@ -1,1 +1,6 @@
+original_prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
 
+print("Старый список:", original_prices)
+
+new_list = [(i if i > 0 else 0) for i in original_prices]
+print("Новый список:", new_list)
