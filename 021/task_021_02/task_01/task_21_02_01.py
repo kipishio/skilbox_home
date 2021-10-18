f8947a9f02502c8ebae519cfb8081e76e1119b83
BuildTools <unconfigured@null.spigotmc.org>
2021-10-18 23:38:59 +0500

    Домашняя работа №21 task-02 задача 1

---
+++ 021/task_021_02/task_01/task_21_02_01.py
@@ -0,0 +1,10 @@
+def factorial(numb):
+    if numb == 1:
+        return 1
+    return numb * factorial(numb - 1)
+
+
+
+fact = int(input('Введите число факториал которого хотели бы узнать: '))
+result_factorial = factorial(fact)
+print(result_factorial)
