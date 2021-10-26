0ed7e630c43e2f9e585a8b2d9bd1342077ea231a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-27 01:20:31 +0500

    Домашняя работа №22 task-02 приступил к решению

---
+++ 022/task_22_02/task_01/task_22_02_01.py
@@ -0,0 +1,17 @@
+import os
+
+
+def path_check(name):
+    name = os.path.abspath(name)
+    if os.path.exists(name):
+        if os.path.isdir(name):
+            print('Путь {0} указывает на дирикторию'.format(name))
+        elif os.path.isfile(name):
+            print('Путь {0} указывает на файл.'.format(name))
+            print('Размер файла {0} байт'.format(os.path.getsize(name)))
+    else:
+        print('Пути не существует')
+
+path_end_name = os.path.join('eame', 'eame2', 'eew_text.txt')
+path_check(path_end_name)
+
