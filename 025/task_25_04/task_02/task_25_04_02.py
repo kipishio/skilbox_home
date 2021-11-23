e24b2e592a3aa214b5280a63b036fecc6fe1a6e1
BuildTools <unconfigured@null.spigotmc.org>
2021-11-24 01:51:13 +0500

    Home work №25 task- 4 02 edit

--- 025/task_25_04/task_02/task_25_04_02.py
+++ 025/task_25_04/task_02/task_25_04_02.py
@@ -1,1 +1,42 @@
-# TODO
+class Can_Fly:
+    def __init__(self):
+        self.__height = 0
+        self.__speed = 0
+
+    def take_off(self):
+        pass
+
+    def fly(self):
+        pass
+
+    def to_land(self):
+        self.__height = 0
+        self.__speed = 0
+
+    def get_hight(self):
+        return self.__height
+
+    def set_hight(self, hight):
+        self.__height = hight
+
+    def get__speed(self):
+        return self.__speed
+
+    def set_speed(self, speed):
+        self.__speed += speed
+
+
+
+class Butterfly(Can_Fly):
+    def __init__(self):
+        super().__init__()
+
+    def take_off(self):
+        self.set_hight(1)
+
+    def fly(self):
+        self.set_speed(0.5)
+
+    def land(self):
+        self.__height = 0
+        self.__speed = 0
