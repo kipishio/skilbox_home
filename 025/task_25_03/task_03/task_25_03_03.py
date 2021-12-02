26ec69a4c1dc00a71e9ceedb660eafd0a9cc6286
kipishio <kipishio@gmail.com>
2021-12-02 16:19:39 +0500

    Vipolnenie domashney raboti №25 task_04 02 perfomed

--- 025/task_25_03/task_03/task_25_03_03.py
+++ 025/task_25_03/task_03/task_25_03_03.py
@@ -9,11 +9,13 @@ with open('numbers.txt', 'r', encoding='utf-8') as file:
         list_numbers = i_line.split(' ')
         dividend = int(list_numbers[0].strip())
         divider = int(list_numbers[1].strip())
-        if dividend < divider:
-            raise My_DivisionError('Нельзя делить большее на меньшее пер dividend/divider = {} / {}'.format(dividend, divider))
-        else:
-            try:
-                nn = dividend / divider
-            except ZeroDivisionError:
-                print('На ноль делить нельзя')
-
+        try:
+            if dividend < divider:
+                raise My_DivisionError
+            else:
+                try:
+                    nn = dividend / divider
+                except ZeroDivisionError:
+                    print('На ноль делить нельзя')
+        except My_DivisionError:
+            print('Нельзя делить большее на меньшее пер dividend/divider = {} / {}'.format(dividend, divider))
---
+++ 026/task_26_02/task_01/task_26_02_01.py
@@ -0,0 +1,4 @@
+# TODO приступим
+
+def sdfsf():
+    pass
