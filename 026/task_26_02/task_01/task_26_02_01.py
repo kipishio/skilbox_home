685e48259aac3ec56c27427c3aa397bde2a174f4
kipishio <kipishio@gmail.com>
2021-12-03 16:03:17 +0500

    Vipolnenie domashney raboti №25 task_04 02 perfomed

--- 026/task_26_02/task_01/task_26_02_01.py
+++ 026/task_26_02/task_01/task_26_02_01.py
@@ -1,4 +1,39 @@
 # TODO приступим
 
-def sdfsf():
-    pass
+# my_list = [1, 2, 3, 4, 5]
+# iterarot = iter(my_list)
+#
+# def iter_list(my_iter):
+#     while True:
+#         try:
+#             print(next(my_iter))
+#         except StopIteration:
+#             # print('Конец')
+#             break
+#
+#
+#
+# iter_list(iterarot)
+
+class my_kl:
+
+    def __init__(self):
+        self.cur_val = 0
+        self.next_val = 1
+
+    def next(self):
+        self.cur_val, self.next_val = self.next_val, self.cur_val + self.next_val
+        print(self.cur_val)
+
+
+rty = my_kl()
+
+rty.next()
+rty.next()
+rty.next()
+rty.next()
+rty.next()
+rty.next()
+rty.next()
+rty.next()
+rty.next()
