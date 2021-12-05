9e4abb6806f6eda35505821e7dc8f3ef43fb0531
BuildTools <unconfigured@null.spigotmc.org>
2021-12-05 17:49:00 +0500

    Home work №26 task-6 02 perfomed

--- 026/task_26_05/task_02/task_26_05_02.py
+++ 026/task_26_05/task_02/task_26_05_02.py
@@ -1,1 +1,11 @@
-# TODO
+def reade_file():
+    with open('readmе.text', 'r', encoding='utf-8') as file:
+        for i_line in file:
+            data = i_line.strip().split(' ')
+            if len(data) == 1 and data[0] == '':
+                pass
+            else:
+                yield len(data)
+
+
+print('Всего чисел в файле {count}'.format(count=sum(reade_file())))
