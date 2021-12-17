905bbde31705247f88b75ebf121511d6cb691ef3
BuildTools <unconfigured@null.spigotmc.org>
2021-12-18 03:20:40 +0500

    Home work №27 task-2 01 02 perfomed

--- 027/task_27_02/task_01/task_27_02_01.py
+++ 027/task_27_02/task_01/task_27_02_01.py
@@ -1,1 +1,23 @@
-#TODO решить
+from typing import Callable, Any
+
+def func_1(x:int)-> int:
+    return x + 10
+
+def pr_my(text: str, number: int)-> str:
+    t_my = text * number
+    print(type(t_my))
+    return t_my
+
+def  func_2(func: Callable, *args, **kwargs)-> Any:
+    result = func(*args, **kwargs)
+    if  type(result) == int:
+        return result * result
+    elif type(result) == str:
+        return result
+
+
+print(func_2(func_1, 9))
+
+print(func_2(pr_my, 'привет ', 5))
+
+
--- 027/task_27_02/task_02/task_27_02_02.py
+++ 027/task_27_02/task_02/task_27_02_02.py
@@ -1,1 +1,28 @@
-#TODO решить
+from typing import Callable, Any
+
+import time
+
+
+def test_time_work(func: Callable, *args, **kwargs)->str:
+    time_start = time.time()
+    func(*args, **kwargs)
+    time_end = time.time()
+    time_result = time_end - time_start
+    return 'Время работы программы {time_work}'.format(time_work=round(time_result, 3))
+
+
+def kil_time(numb: int)->None:
+    result = sum([i ** 10 for i in range(1, numb)])
+
+    print('сумма чисел списка {}'.format(result, 2))
+
+
+print(test_time_work(kil_time, 99999))
+
+
+my_ddd = test_time_work(kil_time, 2)
+
+print('')
+
+
+input()
