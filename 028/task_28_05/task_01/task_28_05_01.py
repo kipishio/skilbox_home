30b9fbc5512ac7762c734b47c4fa338d20488af3
BuildTools <unconfigured@null.spigotmc.org>
2021-12-23 13:25:05 +0500

    Home work №28 task-5 01 02 perfomed

--- 028/task_28_05/task_01/task_28_05_01.py
+++ 028/task_28_05/task_01/task_28_05_01.py
@@ -1,1 +1,20 @@
-#TODO Решить
+import _io
+
+
+class File:
+    def __init__(self, file_name: str, mode: str) -> None:
+        self.file_name = file_name
+        self.mode = mode
+
+    def __enter__(self) -> _io.TextIOWrapper:
+        self.file = open(self.file_name, self.mode, encoding='utf-8')
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.file.close()
+
+
+with File('example.txt', 'w') as file:
+    print(type(file))
+    print(file.__class__)
+    file.write('Всем привет!\n')
--- 028/task_28_05/task_02/task_27_05_02.py
+++ 028/task_28_05/task_02/task_27_05_02.py
@@ -1,1 +1,33 @@
-#TODO Решить
+import traceback
+
+class Example:
+
+    def __init__(self):
+        print('Вызов __init__')
+
+
+    def __enter__(self):
+        print('Вызов __enter__')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Вызов __exit__')
+
+        if exc_type:
+            print()
+            print('{} {}'.format('Тип ошибки: ', exc_type))
+            print('Значение ошибки: {exc_val}'.format(exc_val=exc_val))
+            print('"След" ошибки: {exc_tb}'.format(exc_tb=exc_tb))
+
+        return True
+
+my_obj = Example()
+
+with my_obj as obj:
+
+    print('Код внутри первого вызова контекст менеджера')
+
+    with my_obj as obj2:
+        raise Exception('Выброс исключения во вложенном (втором) вызове контекст менеджере')
+
+    print('Я обязательно выведусь...')
