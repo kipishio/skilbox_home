263fe57813a7cde0db7ddf483c6a5e0c821568d9
kipishio <kipishio@gmail.com>
2022-01-17 09:25:28 +0500

    module 29 - 03 edit

--- 029/Home/task_03.py
+++ 029/Home/task_03.py
@@ -1,33 +1,33 @@
-from typing import Callable
 from datetime import datetime
 import functools
 
 
-def time_work(func, format):
-    # @functools.wraps(func)
+def time_work(func, format_):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
-
         start = datetime.now()
         print("- Запускается '{inf}' . Дата и время запуска: {dt}".format(inf=str(func).split()[1],
-                                                                          dt=start.strftime(format)))
+                                                                          dt=start.strftime(format_)))
 
         result = func(*args, **kwargs)
 
         end = datetime.now() - start
         print("- Завершение '{inf}' , время работы = {dt}s".format(inf=str(func).split()[1],
-                                                                          dt=round(end.total_seconds(), 3)))
+                                                                   dt=round(end.total_seconds(), 3)))
 
         return result
+
     return wrapper
 
-def log_methods(format_, var_func):
 
+def log_methods(format_, var_func):
     def decor_func(cls):
         for i_named_func in dir(cls):
-            if i_named_func.startswith('__') == False:
+            if i_named_func.startswith('__') is False:
                 dec_func = var_func(getattr(cls, i_named_func), format_)
                 setattr(cls, i_named_func, dec_func)
         return cls
+
     return decor_func
 
 
@@ -59,22 +59,7 @@ class B(A):
 
         return result
 
+
 my_obj = B()
 my_obj.test_sum_1()
 my_obj.test_sum_2()
-
-
-# Результат:#
-# - Запускается 'B.test_sum_1'. Дата и время запуска: Apr 23 2021 - 21:50:37
-# - Запускается 'A.test_sum_1'. Дата и время запуска: Apr 23 2021 - 21:50:37
-#
-# Тут метод test_sum_1
-# - Завершение 'A.test_sum_1', время работы = 0.187s
-#
-# Тут метод test_sum_1 у наследника
-# - Завершение 'B.test_sum_1', время работы = 0.187s
-#
-# - Запускается 'B.test_sum_2'. Дата и время запуска: Apr 23 2021 - 21:50:37
-# Тут метод test_sum_2 у наследника
-#
-# - Завершение 'B.test_sum_2', время работы = 0.370s
