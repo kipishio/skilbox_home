87ac5482c10cb8a9df03dc71f0be213453b4faf4
BuildTools <unconfigured@null.spigotmc.org>
2022-01-18 02:12:57 +0500

    module 30 - тест+ 02-е задание

--- 030/task_30_02/task_01/task_30_02_01.py
+++ 030/task_30_02/task_01/task_30_02_01.py
@@ -1,1 +1,32 @@
-#TODO решить
+import functools
+import locale
+from typing import Callable
+
+counter = dict()
+
+def log_decor(func):
+	counter = dict()
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		nonlocal counter
+		result = func(*args, **kwargs)
+		if func.__name__ in  counter:
+			counter[func.__name__] += 1
+		else:
+			counter[func.__name__] = 1
+
+		return result
+
+	return wrapper
+
+@log_decor
+def hello():
+	print('Hello')
+
+hello()
+hello()
+hello()
+hello()
+hello()
+print(counter, '***')
+print(dir())
