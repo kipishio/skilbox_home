a5b6cf0e073b882729a8f7a168268efdd0369119
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 01:09:23 +0500

    module 31 - 04 task_01, EDIT

--- 031/task_30_04/task_01/task_31_04_01.py
+++ 031/task_30_04/task_01/task_31_04_01.py
@@ -1,8 +1,23 @@
 import requests
 import re
-
+import json
 
 if __name__ == '__main__':
-	rq = requests.get('https://swapi.dev/api/planets/3/')
-	print(rq)
-	print(rq.text)
+	rq = requests.get('https://swapi.dev/api/people/3/')
+	#
+	# print(rq)
+	# print(rq.text)
+
+	data = json.loads(rq.text)  # Десериализация JSON
+	data['name'] = 'Роман'
+	# print(data['name'])
+	# print(data['homeworld'])
+	# print(data)
+	with open('my_request_file.json', 'w') as file:
+		json.dump(data, file, indent='    ')  # Сериализация JSON
+
+
+	with open('my_request_file.json', 'r') as file:
+		data = json.load(file)
+		print(data)
+
