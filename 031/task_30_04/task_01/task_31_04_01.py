2577636e9a61d110d04de32f3b6b0affceed373b
kipishio <kipishio@gmail.com>
2022-01-25 17:10:33 +0500

    module 30_04_02 performed

--- 031/task_30_04/task_01/task_31_04_01.py
+++ 031/task_30_04/task_01/task_31_04_01.py
@@ -3,21 +3,30 @@ import re
 import json
 
 if __name__ == '__main__':
-	rq = requests.get('https://swapi.dev/api/people/3/')
-	#
-	# print(rq)
-	# print(rq.text)
+    my_request = requests.get('https://swapi.dev/api/people/3/')
 
-	data = json.loads(rq.text)  # Десериализация JSON
-	data['name'] = 'Роман'
-	# print(data['name'])
-	# print(data['homeworld'])
-	# print(data)
-	with open('my_request_file.json', 'w') as file:
-		json.dump(data, file, indent='    ')  # Сериализация JSON
+    data = json.loads(my_request.text)  # Десериализация JSON
+    data['name'] = 'Roman'
+
+    with open('my_request_file.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent='    ')  # Сериализация JSON
+
+    with open('my_request_file.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)          # Десериализация JSON
+        data['height'] = 599999
+        data_in_str = json.dumps(data, indent='    ') # Сериализация
+
+    print(data)
+    print(data_in_str)
+    print('\n\n')
 
 
-	with open('my_request_file.json', 'r') as file:
-		data = json.load(file)
-		print(data)
+    my_request = requests.get('https://swapi.dev/api/films/?search=A New Hope')
+    data = json.loads(my_request.text)
 
+    print(data)
+
+    str_data = json.dumps(data, indent='    ')
+    print('тут инфа по вильму:\n',str_data)
+    print('\n\n')
+    print('Тут текст и пред история по фильму', data['results'][0]['opening_crawl'])
